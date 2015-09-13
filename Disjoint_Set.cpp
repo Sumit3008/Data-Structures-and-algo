36d6c9abe509a12823cb4035c54e98d498babece
Sumit3008 <sumit0892@ymail.com>
2015-09-14 01:34:40 +0530

    Update Disjoint_Set.cpp

--- Disjoint_Set.cpp
+++ Disjoint_Set.cpp
@@ -14,6 +14,7 @@ int find(int val){
 
 void merge(int ele1, int ele2){
 	int root1 = find(ele1), root2 = find(ele2);	//finding what tree both elements belong to
+	if(root1 == root2) return;
 	if(a[root1] < a[root2]){	//merge root2 into root1 (because they are negative no's)
 		a[root1] += a[root2];	//adding the count of the 2 trees
 		a[root2] = root1;	//changing the parent pointer
